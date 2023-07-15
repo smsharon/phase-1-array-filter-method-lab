9127a0884af89d47933d011848d26f876ee46e8f
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-07-15 02:43:37 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(names, query) {
+    return names.filter(name => name.toLowerCase() === query.toLowerCase());
+  }
+
+  function fuzzyMatch(names, query) {
+    return names.filter(name => name.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
